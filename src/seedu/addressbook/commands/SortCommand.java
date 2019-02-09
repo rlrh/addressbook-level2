e52ce95bc751f729cfade4641a06b59eaf55b438
rlrh <rlrh1996@gmail.com>
2019-02-09 23:40:29 +0800

    Modify sort command usage message to reflect updated implementation

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -16,7 +16,7 @@ public class SortCommand extends Command {
     public static final String COMMAND_WORD = "sort";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Sorts all persons in the address book in alphabetical order.\n"
+            + ": Sorts last shown persons in the address book in alphabetical order.\n"
             + "Example: " + COMMAND_WORD;
 
 
